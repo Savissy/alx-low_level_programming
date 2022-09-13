107c9a515c33db59a77bc0f038d4d0e33c9d6535
savissy <uzoukwusaviour@gmail.com>
2022-09-13 15:33:13 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - print numbers in in small letter alphabetically
+*
+* Description: using the main function
+* this program prints " programming can be alphabetical "
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
