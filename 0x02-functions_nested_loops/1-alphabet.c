2623a37c32611f4225b66fe7cfa73f8da241d9bd
savissy <uzoukwusaviour@gmail.com>
2022-09-13 22:11:05 -0700

    print alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 * print_alphabet - Entry point
-* Return:0
+* Return: Always 0 (success)
 */
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
+char i;
+for (i = 'a'; i <= 'z'; i++)
 {
-_putchar(c);
+_putchar(i);
 }
 _putchar('\n');
 }
