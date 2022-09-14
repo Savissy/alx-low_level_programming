18f4d55c6492ecfa01aa5ceccefa55a73c7a68d2
savissy <uzoukwusaviour@gmail.com>
2022-09-13 21:11:58 -0700

    10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /**
+* main - prints alphabets in 10 times
 * print_alphabet_x10 - prints 10 times the alphabet, in lower case
 * followed by a new line
+* Return: Always 0 (success)
 */
-void print_alphabet_x10(void)
+int main(void)
 {
+void print_alphabet_x10(void);
 char ch;
 int i;
 i = 0;
@@ -18,5 +22,6 @@ ch++;
 }
 putchar('\n');
 i++;
+return (0);
 }
 }
