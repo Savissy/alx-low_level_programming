e6b100b4f42296d72def7f8b69063af1b78f8cda
savissy <uzoukwusaviour@gmail.com>
2022-09-16 03:22:58 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,10 @@ i = 0;
 
 /*while (i < 10) */
 /*{ */
-/* putchar(i);*/
+/*	putchar(i);*/
 /*} */
 
-printf("infinite loop avoided! \o/\n");
+printf("infinite loop avoided! \\o/\n");
 
 return (0);
 }
