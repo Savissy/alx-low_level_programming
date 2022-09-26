6150d2499e37dcf06662d3f45b990447e7744d5d
savissy <uzoukwusaviour@gmail.com>
2022-09-26 14:34:03 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ int i;
 unsigned int sum, sum1;
 for (i = 0; i < size; i++)
 {
-sum += a[(size * 1) + i];
+sum += a[(size * i) + i];
 sum1 += a[(size * (i + 1)) - (i + 1)];
 }
 printf("%d, %d\n", sum, sum1);
