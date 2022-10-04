3a8e02148402fd337fa669aaea3ee13947eb0685
savissy <uzoukwusaviour@gmail.com>
2022-10-04 14:05:20 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,22 +6,23 @@
 * a specific character
 * @c: character to be used for initialization
 * @size: ssiize of array to be created
-* Return: pointer to array,
-* NULL if fails
+* Return: pointer to array (success), NULL (Error)
 */
 char *create_array(unsigned int size, char c)
 {
-int i = 0;
-char *str = NULL;
-str = malloc(size * sizeof(char));
+unsigned int i = 0;
+char *str;
+
+str = (char *) malloc(size * sizeof(char));
+if (str == NULL)
+return (0);
+if (size == 0)
+return (NULL);
 while (i < size)
 {
 str[i] = c;
 i++;
 }
-if (size == 0)
-{
-return (NULL);
-}
+str[i] = '\0';
 return (str);
 }
