5f0e62f83ff91d4906eeaded097ef7bc6a44e23e
savissy <uzoukwusaviour@gmail.com>
2022-10-11 02:19:06 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 if (d->age <= 0)
-d->age = 0;
+d->age = 0.0;
 printf("Name: %s\nAge: %f\nowner: %s\n", d->name, d->age, d->owner);
 }
