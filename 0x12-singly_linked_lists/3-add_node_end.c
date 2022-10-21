294624ed5abc7b6306d66a2fb7214b1e9870cff9
savissy <uzoukwusaviour@gmail.com>
2022-10-21 11:32:13 -0700

    node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strlen - get the string's length
+* @str: string to get length of
+* Return: string's length
+*/
+unsigned int _strlen(char *str)
+{
+int i = 0;
+while (*str)
+{
+str++;
+i++;
+}
+return (i);
+}
+
+/**
+* add_node_end - add node at the end of a list
+* @head: double pointer
+* @str: string to add new node
+* Return: nodes to the end
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *newnode, *x;
+if (str == NULL)
+return (NULL);
+newnode = malloc(sizeof(list_t));
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->str = strdup(str);
+newnode->next = NULL;
+if (*head == NULL)
+{
+*head = newnode;
+return (newnode);
+}
+x = *head;
+while (x->next)
+x = x->next;
+x->next = newnode;
+return (newnode);
+}
